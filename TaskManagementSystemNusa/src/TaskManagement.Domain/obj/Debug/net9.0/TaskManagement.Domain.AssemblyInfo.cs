c90ad935357f28e24c29c9331a0c63032f51768e
dhnugroho.dev <20092353+dhnugroho@users.noreply.github.com>
2025-03-12 08:46:03 +0700

    Tested

--- TaskManagementSystemNusa/src/TaskManagement.Domain/obj/Debug/net9.0/TaskManagement.Domain.AssemblyInfo.cs
+++ TaskManagementSystemNusa/src/TaskManagement.Domain/obj/Debug/net9.0/TaskManagement.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagement.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9b5fb1941f1703b27fb4f56bb3d5cd7055c6324")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagement.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagement.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
